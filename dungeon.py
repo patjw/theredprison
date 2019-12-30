d16cb1bc098871edb7db8b2ad654d12fc414f925
patjw <patrickjosephwilson@gmail.com>
2019-12-31 07:48:35 +1000

    initial commit

---
+++ dungeon.py
@@ -0,0 +1,212 @@
+class Dungeon_Branch:
+	#a container class for dungeon branches
+	def __init__(self, name, full_map=False, depth=1, theme=None, vault_type='vaults', random_populate=True, random_placement=True, overworld=False, always_lit=False, random_torches=True, special_vaults=True, random_quests=False, roughen_map=True, is_safe=False, can_recruit=True):
+		self.name = name
+		self.full_map = full_map
+		self.depth = depth
+		self.theme = theme
+		self.vault_type=vault_type
+		self.random_populate=random_populate
+		self.random_placement=random_placement
+		self.overworld=overworld #special flag which stops monster and item generation and prevents followers from spawning on this map
+		self.always_lit=always_lit
+		self.random_torches=random_torches
+		self.special_vaults=special_vaults
+		self.random_quests=random_quests
+		self.roughen_map=roughen_map
+		self.is_safe = is_safe
+		self.can_recruit = can_recruit
+		
+		self.monsters = [None for i in range(depth)] #empty container for specifying monster types per dungeon level - should default to full monster generation if no corresponding entry for depth
+		self.encounters = [None for i in range(depth)] #empty container for item types as above
+		self.items = [None for i in range(depth)] #empty container for item types as above
+		
+		### NOTE: above containers need a dictionary for each level of the available depth even if it's empty, otherwise it'll default to generating all entity types. the key to the dictionary should be the entity type and the value associated with the key should be the weighting to use for generation
+		
+		### NOTE: the below containers work differently - you don't need to specify a dictionary for each level, rather the key is the dungeon level and the value is the function to use to generate that entity somewhere on the level
+		
+		self.must_generate_monster = {} #empty container which will force certain monsters to be generated on certain levels
+		self.must_generate_encounter = {} #same, but for encounters
+		self.must_generate_item = {} #same, but for items
+		
+dungeon = []
+
+### NOTE: need to overwrite monster, item and encounter generation for each level with empty dictionaries if you do not want the default generator to apply. null values for any for that depth will default to the standard generator for that category
+
+branch = Dungeon_Branch('overworld', full_map=True, theme='overworld', random_populate=False, random_placement=False, overworld=True, always_lit=True, random_torches=False)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('warrens', depth=3, vault_type='caves', special_vaults=False)
+branch.monsters = [{'kobold':10, 'goblin':1, 'giant_bat':1, 'hyena':1, 'jackal':1}, #level 1
+{'kobold':10, 'orc':1, 'hobgoblin':1, 'bugbear':1, 'hyena':1, 'jackal':1}, #level 2
+{'kobold':5, 'orc':1, 'hobgoblin':1, 'bugbear':1, 'gnoll':1}] #level 3
+branch.encounters = [{}, #level 1
+{'basic kobold':1}, #level 2
+{'basic kobold':3}] #level 3
+#branch.items = []
+#branch.must_generate_monster = {}
+branch.must_generate_encounter = {3:['kobold horde']}
+branch.must_generate_item = {3:['rare_magic']}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('ancient_shrine', depth=3, vault_type='vaults', special_vaults=False)
+branch.monsters = [{'skeleton':10, 'zombie':5}, #level 1
+{'skeleton':5, 'zombie':10, 'ghoul':5}, #level 2
+{'skeleton':3, 'zombie':3, 'ghoul':3, 'ghost':3}] #level 3
+branch.encounters = [{}, #level 1
+{'cultists':5}, #level 2
+{'basic undead':3, 'cultists':3}] #level 3
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+branch.must_generate_item = {3:['rare_magic']}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('bandit_keep', depth=3, vault_type='vaults', special_vaults=False)
+branch.monsters = [{'bandit':10, 'thug':3}, #level 1
+{'bandit':10, 'thug':5, 'spy':2}, #level 2
+{'bandit':5, 'thug':5, 'spy':5, 'assassin':5}] #level 3
+branch.encounters = [{}, #level 1
+{'basic bandit':3}, #level 2
+{'basic bandit':5}] #level 3
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+branch.must_generate_item = {3:['rare_magic']}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('sanctum', depth=3, vault_type='vaults', special_vaults=False)
+branch.monsters = [
+{'blink_dog':1, 'blood_hawk':1, 'giant_rat':1, 'hippogriff':1}, #level 1
+{'flying_sword':1, 'flying_snake':1, 'animated_armour':1, 'gargoyle':1}, #level 2
+{'mage':1, 'air_elemental':1, 'fire_elemental':1, 'earth_elemental':1}] #level 3
+branch.encounters = [{},{},{}]
+#branch.items = []
+branch.must_generate_monster = {3:['archmage']}
+#branch.must_generate_encounter = {}
+branch.must_generate_item = {3:['rare_magic']}
+dungeon.append(branch)
+
+branch = Dungeon_Branch("haunt", depth=3, vault_type='vaults', special_vaults=False)
+branch.monsters = [{'skeleton':3, 'zombie':3, 'raven':1, 'hyena':1, 'bat':1, 'spider':1, 'flying_sword':1}, #level 1
+{'skeleton':2, 'zombie':2, 'animated_armour':1, 'flying_sword':1, 'death_dog':1, 'ghoul':2}, #level 2
+{'animated_armour':1, 'flying_sword':1, 'flameskull':1, 'death_dog':1, 'hell_hound':1, 'ghoul':1, 'mummy':1}] #level 3
+branch.encounters = [{}, #level 1
+{'basic undead':1}, #level 2
+{'basic undead':3}] #level 3
+#branch.items = []
+branch.must_generate_monster = {3:['aoife']}
+#branch.must_generate_encounter = {}
+branch.must_generate_item = {3:['rare_magic']}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('badbog', depth=3, vault_type='caves', special_vaults=False)
+branch.monsters = [{'boar':1, 'poisonous_snake':1, 'frog':1, 'scorpion':1, 'spider':1, 'stirge':1}, #level 1
+{'boar':1, 'giant_boar':1, 'poisonous_snake':1, 'giant_poisonous_snake':1, 'giant_frog':1, 'giant_scorpion':1, 'giant_spider':1, 'giant_wolf_spider':1}, #level 2
+{'giant_boar':1, 'giant_poisonous_snake':1, 'giant_frog':1, 'giant_toad':1, 'giant_spider':1, 'giant_wolf_spider':1, 'giant_crocodile':1}] #level 3
+branch.encounters = [{}, {}, {}]
+#branch.items = []
+branch.must_generate_monster = {3:['hydra']}
+#branch.must_generate_encounter = {}
+branch.must_generate_item = {3:['rare_magic']}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('vierfort', full_map=True, theme='town', always_lit=True, random_populate=False, random_torches=False)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('catacombs', depth=3, vault_type='vaults', special_vaults=False)
+branch.monsters = [{'gnoll':2, 'bugbear':2, 'lizardfolk':5}, #level 1
+{'gnoll':4, 'bugbear':4, 'lizardfolk':2}, #level 2
+{'gnoll':4, 'bugbear':4, 'hill_giant':2}] #level 3
+branch.encounters = [{}, {}, {}]
+#branch.items = []
+branch.must_generate_monster = {3:['flameskull', 'flameskull', 'flameskull']}
+#branch.must_generate_encounter = {}
+branch.must_generate_item = {3:['rare_magic']}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('rage_hill', depth=3, vault_type='caves', special_vaults=False, can_recruit=False)
+branch.monsters = [{'orc':2, 'goblin':2, 'hobgoblin':2, 'tribal_warrior':2, 'brown_bear':1},
+{'orc':2, 'tribal_warrior':2, 'berserker':2, 'ogre':2, 'black_bear':1},
+{'berserker':2, 'ogre':2, 'minotaur':2, 'sabre_toothed_tiger':1}]
+branch.encounters = [{},{},{'solo ogre mage':1}]
+#branch.items = []
+branch.must_generate_monster = {3:['fire_giant', 'frost_giant', 'cyclops']}
+#branch.must_generate_encounter = {}
+branch.must_generate_item = {3:['rare_magic']}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('red_outpost', full_map=True, theme='town', always_lit=True, random_populate=False, random_torches=False, is_safe=True)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('red_prison', theme='red', random_quests=True, depth=10)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+branch.must_generate_monster = {10:['adult_red_dragon']}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('north_warren', full_map=True, theme='town', always_lit=True, random_populate=False, random_torches=False, is_safe=True)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('mirefield_keep', full_map=True, theme='town', always_lit=True, random_populate=False, random_torches=False, is_safe=True)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('beggars_hole', full_map=True, always_lit=True, random_populate=False, random_torches=False, is_safe=True)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('black_hollow', full_map=True, theme='town', always_lit=True, random_populate=False, random_torches=False, is_safe=True)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
+
+branch = Dungeon_Branch('cindemere', full_map=True, theme='town', always_lit=True, random_populate=False, random_torches=False, is_safe=True)
+#branch.monsters = []
+#branch.encounters = []
+#branch.items = []
+#branch.must_generate_monster = {}
+#branch.must_generate_encounter = {}
+#branch.must_generate_item = {}
+dungeon.append(branch)
---
+++ encounters.py
@@ -0,0 +1,122 @@
+# coding: utf8
+
+class Encounter: #container class for list of monsters container class
+
+	def __init__(self, name, faction, cr):
+		self.name = name
+		self.monsters = [] #this will be a list of monsters objects
+		self.count = 0
+		self.faction = faction
+		self.cr = cr
+		
+	def add_group(self, group):
+		self.monsters.append(group)
+		
+	def next_group(self):
+		if self.count < len(self.monsters):
+			self.count += 1
+			return self.monsters[self.count-1]
+		else:
+			self.count = 0
+			return None
+			
+class Monsters: #container class for instructions to build each monster type in encounter
+
+	def __init__(self, type, name=None, min=2, max=5, str=None, dex=None, con=None, int=None, wis=None, cha=None, clvl=None, magic=False, spells=None, ranged=False, illumination_chance=0):
+		self.type = type
+		self.name = name
+		self.min = min
+		self.max = max
+		self.str = str
+		self.dex = dex
+		self.con = con
+		self.int = int
+		self.wis = wis
+		self.cha = cha
+		self.clvl = clvl
+		self.magic = magic
+		self.spells = spells
+		self.ranged = ranged
+		self.illumination_chance = illumination_chance #percentage chance to carry a torch
+		
+encounters = []
+
+###
+### CR 1 ENCOUNTERS
+###
+
+#kobold mob w/ mages, shamans and slingers
+enc = Encounter('basic kobold', 'kobold', 1)
+enc.add_group(Monsters(type='kobold', min=1, max=3))
+enc.add_group(Monsters(type='kobold', name='kobold slinger', min=0, max=2, ranged=True))
+enc.add_group(Monsters(type='kobold', name='kobold mage', min=0, max=1, int=12, clvl=4, magic=True, spells=['fire bolt', 'magic missile']))
+enc.add_group(Monsters(type='kobold', name='kobold shaman', min=0, max=1, wis=14, clvl=4, magic=True, spells=['sacred flame', 'cure wounds']))
+for mon in enc.monsters: mon.illumination_chance = 25
+encounters.append(enc)
+
+#simple bandit mob w/ a captain
+enc = Encounter('basic bandit', 'bandit', 1)
+enc.add_group(Monsters(type='bandit', min=2, max=4))
+enc.add_group(Monsters(type='bandit_captain', min=1, max=1))
+for mon in enc.monsters: mon.illumination_chance = 75
+encounters.append(enc)
+ 
+#group of cultists
+enc = Encounter('cultists', 'undead', 1)
+enc.add_group(Monsters(type='cultist', min=2, max=5))
+enc.add_group(Monsters(type='cult_fanatic', min=1, max=2))
+for mon in enc.monsters: mon.illumination_chance = 75
+encounters.append(enc)
+
+#simple undead mob with mix of skeletons, zombies and ghouls
+enc = Encounter('basic undead', 'undead', 1)
+enc.add_group(Monsters(type='skeleton', min=0, max=2))
+enc.add_group(Monsters(type='zombie', min=0, max=2))
+enc.add_group(Monsters(type='ghoul', min=0, max=2))
+encounters.append(enc)
+
+#lizard squad 
+enc = Encounter('basic lizardfolk', 'lizardfolk', 1)
+enc.add_group(Monsters(type='lizardfolk', min=1, max=2))
+enc.add_group(Monsters(type='lizard', min=4, max=6))
+encounters.append(enc)
+
+###
+### CR 2 ENCOUNTERS
+###
+
+#orc mob w/ mages, shamans and chuckers
+enc = Encounter('basic orc', 'orc', 2)
+enc.add_group(Monsters(type='orc', min=1, max=3))
+enc.add_group(Monsters(type='orc', name='orc chucker', min=0, max=2, ranged=True))
+enc.add_group(Monsters(type='orc', name='orc mage', min=0, max=1, int=14, clvl=5, magic=True, spells=['fire bolt', 'magic missile']))
+enc.add_group(Monsters(type='orc', name='orc shaman', min=0, max=1, wis=16, clvl=5, magic=True, spells=['sacred flame', 'cure wounds']))
+for mon in enc.monsters: mon.illumination_chance = 25
+encounters.append(enc)
+
+###
+### CR 3 ENCOUNTERS
+###
+
+#ogre with goblin followers
+enc = Encounter('basic ogre', 'ogre', 3)
+enc.add_group(Monsters(type='ogre', min=1, max=1))
+enc.add_group(Monsters(type='goblin', min=2, max=4))
+encounters.append(enc)
+
+#ogre mage
+enc = Encounter('solo ogre mage', 'ogre', 3)
+enc.add_group(Monsters(type='ogre', name='ogre mage', min=1, max=1, int=14, clvl=8, magic=True, spells=['fire bolt', 'magic missile', 'fireball']))
+encounters.append(enc)
+
+#kobold horde
+enc = Encounter('kobold horde', 'kobold', 1)
+enc.add_group(Monsters(type='kobold', min=3, max=5))
+enc.add_group(Monsters(type='kobold', name='kobold slinger', min=1, max=3, ranged=True))
+enc.add_group(Monsters(type='kobold', name='kobold mage', min=1, max=2, int=12, clvl=4, magic=True, spells=['fire bolt', 'magic missile']))
+enc.add_group(Monsters(type='kobold', name='kobold shaman', min=1, max=2, wis=14, clvl=4, magic=True, spells=['sacred flame', 'cure wounds']))
+enc.add_group(Monsters(type='nubnag', min=1, max=1))
+for mon in enc.monsters: mon.illumination_chance = 25
+encounters.append(enc)
+
+
